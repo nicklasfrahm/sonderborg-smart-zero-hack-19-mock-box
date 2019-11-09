bbee7e793caab920cdff1a789aabb27c8db1ee31
nicklasfrahm <nicklas.frahm@gmail.com>
2019-11-09 19:26:14 +0100

    Send first mock data to server

---
+++ lib/HttpsClient/HttpsClient.hpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiServerSecureBearSSL.h>
+
+class HttpsClient
+{
+public:
+  HttpsClient(const char apiUri[], const char apiKey[]);
+  ~HttpsClient(void);
+  time_t setClock(void);
+  int request(const char method[], const char endpoint[], String &payload);
+
+private:
+  String apiUri;
+  String apiKey;
+  BearSSL::X509List *certList;
+  BearSSL::WiFiClientSecure *client;
+  BearSSL::Session *session;
+  HTTPClient *https;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "secrets.hpp"
+#include "HttpsClient.hpp"
 
 String animationChars[] = {"/", "-", "\\", "|"};
 int animationCounter = 0;
+
+HttpsClient client(apiUri, apiKey);
 
 void setup()
 {
   // Initialize serial console.
   Serial.begin(115200);
+
+  // Initialize random seed.
+  randomSeed(analogRead(0));
 
   // Configure WiFi connection.
   WiFi.begin(ssid, pass);
@@ -26,16 +32,13 @@ void setup()
   // Print basic info.
   Serial.print("API URI: ");
   Serial.println(apiUri);
-
-  // Print idle state.
-  Serial.print("Idle: ");
 }
 
 void loop()
 {
-  // Display minor animation after successful connection.
-  Serial.printf("%s", animationChars[animationCounter].c_str());
-  animationCounter = (animationCounter + 1) % 4;
-  delay(100);
-  Serial.print("\b");
+  // Make HTTP request.
+  client.setClock();
+  String payload = "{\"data\":[{\"dataSourceId\":\"sensorbox0\",\"deviceId\": \"swipbox0\",\"values\": [{\"unit\": \"dB\",\"value\":86}]}]}";
+  Serial.printf("%d POST /data\n", client.request("POST", "/data", payload));
+  delay(5000);
 }
