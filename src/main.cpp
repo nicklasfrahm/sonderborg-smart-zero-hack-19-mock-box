907c1105c4a912ee67d8c43300e45c9a42814ff0
nicklasfrahm <nicklas.frahm@gmail.com>
2019-11-10 10:24:11 +0100

    Use deep sleep to save energy

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,16 @@
 #include "secrets.hpp"
 #include "BlueDot_BME680.h"
 
-const char deviceId[] = "swipbox0";
+const char DEVICE_ID[] = "swipbox0";
+const int LIGHT_SENSOR_PIN = A0;
+const int BUTTON_PIN = 14; // D5
+const int DOOR_PIN = 12;   // D6
+const int LED_PIN = 13;    // D7
 
 String animationChars[] = {"/", "-", "\\", "|"};
 int animationCounter = 0;
 
-HttpsClient client(apiUri, apiKey);
+HttpsClient client(API_URI, API_KEY);
 BlueDot_BME680 bme = BlueDot_BME680();
 
 void setup()
@@ -20,7 +24,7 @@ void setup()
 
   // Start and configure I2C.
   Wire.begin();
-  bme.parameter.I2CAddress = 0x76;
+  bme.parameter.I2CAddress = 0x77;
   bme.parameter.sensorMode = 0b01;
   bme.parameter.IIRfilter = 0b100;
   bme.parameter.humidOversampling = 0b101;
@@ -39,8 +43,14 @@ void setup()
     }
   }
 
+  // Configure I/O.
+  pinMode(LIGHT_SENSOR_PIN, INPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(DOOR_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
   // Configure WiFi connection.
-  WiFi.begin(ssid, pass);
+  WiFi.begin(SSID, PASS);
 
   Serial.print("WiFi connection: ");
   while (WiFi.status() != WL_CONNECTED)
@@ -54,22 +64,33 @@ void setup()
 
   // Print basic info.
   Serial.print("API URI: ");
-  Serial.println(apiUri);
+  Serial.println(API_URI);
 }
 
 void loop()
 {
+  // Open door.
+  digitalWrite(LED_PIN, HIGH);
+  digitalWrite(DOOR_PIN, HIGH);
+
   // Set clock and clear measurements.
   client.setClock();
   bme.writeCTRLMeas();
 
-  // Send measurements.
-  Serial.printf("%d POST /data > gas0\n", client.sendData(deviceId, "gas0", "Ohms", bme.readGas()));
-  Serial.printf("%d POST /data > temp0\n", client.sendData(deviceId, "temp0", "°C", bme.readTempC()));
-  Serial.printf("%d POST /data > hum0\n", client.sendData(deviceId, "hum0", "%", bme.readHumidity()));
-  Serial.printf("%d POST /data > pres0\n", client.sendData(deviceId, "pres0", "hPa", bme.readPressure()));
-  Serial.printf("%d POST /data > alt0\n", client.sendData(deviceId, "alt0", "m", bme.readAltitudeMeter()));
+  // Read light sensor.
+  float lightIntensity = (float)analogRead(LIGHT_SENSOR_PIN) * 100.0 / 1024.0;
 
-  // Don't kill my cellular data volume.
-  delay(1000);
+  // Send measurements.
+  Serial.printf("%d POST /data > gas0\n", client.sendData(DEVICE_ID, "gas0", "Ohms", bme.readGas()));
+  Serial.printf("%d POST /data > temp0\n", client.sendData(DEVICE_ID, "temp0", "°C", bme.readTempC()));
+  Serial.printf("%d POST /data > hum0\n", client.sendData(DEVICE_ID, "hum0", "%", bme.readHumidity()));
+  Serial.printf("%d POST /data > pres0\n", client.sendData(DEVICE_ID, "pres0", "hPa", bme.readPressure()));
+  Serial.printf("%d POST /data > alt0\n", client.sendData(DEVICE_ID, "alt0", "m", bme.readAltitudeMeter()));
+  Serial.printf("%d POST /data > light0\n", client.sendData(DEVICE_ID, "light0", "%", lightIntensity));
+
+  // Lock door.
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(DOOR_PIN, LOW);
+
+  ESP.deepSleep(0);
 }
